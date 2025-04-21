0438c64058f547e40b4e5541f60b5fcb5ee9700b
alishadell2001 <alisahdel2001@gmail.com>
2025-04-21 11:27:44 +0300

    add accessor and muttsor

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -52,4 +53,25 @@ class User extends Authenticatable
     {
         return $this->hasMany(Project::class, 'user_id', 'id');
     }
+    //Accessor Method
+    //$user->profile_photo_url
+    public function getProfilePhotoUrlAttribute()
+    {
+        if ($this->freelancer->profile_photo_path) {
+            return asset('storage/' . $this->freelancer->profile_photo_path);
+        }
+        return asset('images/default-photo.jpg');
+    }
+     // $this->name
+     public function getNameAttribute($value)
+     {
+         return Str::title($value);
+     }
+ 
+     // Mutators
+     // $user->email = "M@Safadi.ps" -> "m@safadi.ps"
+     public function setEmailAttribute($value)
+     {
+         $this->attributes['email'] = Str::lower($value);
+     }
 }
